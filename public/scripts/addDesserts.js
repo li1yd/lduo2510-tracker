3e67b46f1f8a816c0d0f5b61d851664393dc5a6a
li1yd <lily.duong675@gmail.com>
2024-05-29 17:48:01 +1000

    Data Logging Page 1 Styling (Temp)
    Temporary Styling of 1st page of Add Desserts

--- public/scripts/addDesserts.js
+++ public/scripts/addDesserts.js
@@ -1,4 +1,6 @@
 import abstractView from "./abstractView.js";
+import display from '../src/images/display.png';
+import table from '../src/images/table.png';
 
 export default class extends abstractView{
     constructor(){
@@ -10,43 +12,71 @@ export default class extends abstractView{
     async getHtml(){
         this.currentPage = 1; 
 
-        return `
-            <h1> New Entry </h1>
-            
-            <div id="dessertFormContainer" class="container">
+        return ` 
 
-                <!-- PAGE 1 DATA ENTRY -->
-                <div id="page1" class="page active">
-                    <input type="text" id="dessertName" name="dessertName" placeholder="Dessert Name">
-                    <input type="text" id="dessertPrice" name="dessertPrice" placeholder="Price">
-                    <input type="text" id="recommended" name="recommended" placeholder="Recommended Accompaniments">
-                </div>
+        <main>
+            <!-- Title/ Page Numbers --> 
+            <header class="d-flex justify-content-between align-items-center"> 
+                <h1> New Entry </h1>
+                <h2 class="text-end"id="pageNumber">${this.currentPage}/3</h2>
+            </header>
+
+            <!-- NAVIGATION ARROWS -->
+            <div class="d-flex justify-content-between align-items-center">
+                <button id="prevButton"><</button>
+
+                <div id="dessertFormContainer" class="container">
+                    <!-- PAGE 1 DATA ENTRY -->
+                    
+                    <div id="page1" class="page active">
+                        <form>
+                            <div class="row">
+                                <div class="col-sm-7 d-flex flex-column justify-content-center align-items-center">
+
+                                    <!-- DESSERT NAME -->
+                                    <label for="dessertName"> Dessert Name: </label>
+                                    <input type="text" class="input-group mb-3" id="dessertName" name="dessertName" placeholder="chocolate ice cream">
+
+                                    <!-- DESSERT PRICE -->
+                                    <input type="number" class="input-group input-group-sm mb-3" id="price" name="price" placeholder="Price">
+
+                                    <!-- DESSERT IMAGE -->
+                                    <img src="${display}" id="display" alt="Display Case">
+
+                                </div>
+
+                                <div class="col-sm-5 d-flex flex-column justify-content-center align-items-center">
+                                    <label for="dessertImage"> Upload Dessert Image</label>
+                                    <input type="file" id="upload-file" class="form-control mb-2">
+                                    <label for="recommended">Recommended Accompaniments</label>
+                                    <img src="${table}" id="table" alt="Table">
+                                    <input type="text" class="input-group" id="accopmaniments" name="accompaniments" placeholder="tea with a dash of honey">
+                                </div>
+                            </div>
+                        </form>
+                    </div>
 
 
-                <!-- PAGE 2 DATA ENTRY -->
-                <div id="page2" class="page">
-                    <input type="text" id="storeName" name="storeName" placeholder="McDonald's">
-                    <input type="text" id="datePurchased" name="datePurchased" placeholder="Date Purchased">
-                    <input type="text" id="dessertType" name="dessertType" placeholder="Dessert Type">
-                    <input type="text" id="flavour" name="flavour" placeholder="Flavour">
-                    <input type="text" id="acquisition" name="acquisition" placeholder="Acquisition Method">
-                </div>
+                    <!-- PAGE 2 DATA ENTRY -->
+                    <div id="page2" class="page">
+                        <input type="text" id="storeName" name="storeName" placeholder="McDonald's">
+                        <input type="text" id="datePurchased" name="datePurchased" placeholder="Date Purchased">
+                        <input type="text" id="dessertType" name="dessertType" placeholder="Dessert Type">
+                        <input type="text" id="flavour" name="flavour" placeholder="Flavour">
+                        <input type="text" id="acquisition" name="acquisition" placeholder="Acquisition Method">
+                    </div>
 
 
-                <!-- PAGE 3 DATA ENTRY -->
-                <div id="page3" class="page">
-                    <input type="text" id="country" name="country" placeholder="Country of Origin">
-                    <input type="text" id="rating" name="rating" placeholder="Rating">
-                </div>
+                    <!-- PAGE 3 DATA ENTRY -->
+                    <div id="page3" class="page">
+                        <input type="text" id="country" name="country" placeholder="Country of Origin">
+                        <input type="text" id="rating" name="rating" placeholder="Rating">
+                    </div>                
+                </div>        
 
-                <!-- NAVIGATION ARROWS -->
-                <div class="navigation" mt-4>
-                    <button id="prevButton"><</button>
-                    <span id="pageNumber">${this.currentPage}/3</span>
-                    <button id="nextButton">></button>
-                </div>
-
+            <button id="nextButton">></button>
             </div>
+            </main>
         `;
     }
 
@@ -59,19 +89,15 @@ export default class extends abstractView{
     
         
         prevButton.addEventListener("click", () => {
-            if (this.currentPage > 1){
                 this.currentPage--;
                 this.updatePageDisplay();
                 // Add logic to save data from the current page (optional)
-            }
         });
     
         nextButton.addEventListener("click", () => {
-            if (this.currentPage < 3){
                 this.currentPage++;
                 this.updatePageDisplay();
                 // Add logic to save data from the current page (optional)
-            }
         });
     }
 
@@ -81,11 +107,15 @@ export default class extends abstractView{
         console.log("updatePageDisplay called with currentPage:", this.currentPage);
         const pages = document.querySelectorAll("#dessertFormContainer .page");
 
+        // Update Respective Pages
         pages.forEach((page, index) => {
             page.classList.toggle("active", index + 1 === this.currentPage);
         });
 
+        // Change Page Numbers 
         document.getElementById("pageNumber").textContent = `${this.currentPage}/3`;
+
+        // Disable User from accessing >3 or <1 Pages
         document.getElementById("prevButton").disabled = this.currentPage === 1;
         document.getElementById("nextButton").disabled = this.currentPage === 3;
     }
