6b77b3316147a905f06fdf4a4623c80d42ffc9ea
li1yd <lily.duong675@gmail.com>
2024-06-08 17:08:12 +1000

    Added in Modal Confirmation for Submission of Desserts & Improved Form Responsiveness (Mobile)

--- public/scripts/home.js
+++ public/scripts/home.js
@@ -12,7 +12,7 @@ export default class extends abstractView{
         return `
         <div id="homeContainer" class="container">
             <div class="row">
-                <img src="${logo}" alt="Logo" class="img-fluid" id="logo">
+                <img src="${logo}" alt="Logo" id="logo">
                 <p class="fs-5 fs-sm-4 fs-md-3"> 
                     A web application to track your sweet palate! <br> Begin by clicking ‘+’ 
                     to add a new Dessert Entry. Then, view them in My Desserts!  
